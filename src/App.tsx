4511fa943fcf6323685d337ac6f62a63ca80e23f
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-04-12 09:07:05 +0200

    Přidejte do projektu následující závislosti: a.	✏️ React Router V6. 5.	Vytvořte navigace pro následující spránky: a.	/task – zobrazí komponentu Tasks.tsx. b.	/task/:id – zobrazí detail tasku TaskDetail.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import Tasks from "./page/Tasks";
-import Header from "./component/ui/Header";
 import './App.css'
 import {Provider} from "react-redux";
 import store from "./features/store";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Tasks from "./page/Tasks";
+import Header from "./component/ui/Header";
+import TaskDetail from "./page/TaskDetail";
 
 function App() {
   return (
     <Provider store={store}>
-        <Header />
-        <Tasks />
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path={"/task"} element={<Tasks />} />
+                <Route path={"/task/:id"} element={<TaskDetail />} />
+            </Routes>
+        </BrowserRouter>
     </Provider>
   )
 }
