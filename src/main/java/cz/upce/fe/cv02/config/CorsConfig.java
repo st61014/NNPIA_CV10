6650c29d088d1383db2e40703d9fe8e12ec54e35
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-04-17 16:28:52 +0200

    Cors fix

--- src/main/java/cz/upce/fe/cv02/config/CorsConfig.java
+++ src/main/java/cz/upce/fe/cv02/config/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/task/*")
+        registry.addMapping("/task/**")
                 .allowedOrigins("http://127.0.0.1:5173")
                 .allowedMethods("*")
                 .allowedHeaders("*")
